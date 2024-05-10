8446b298bae2e8a9cb7a0f292adaf6fda529761e
baothai123789 <banbao22042004@gmail.com>
2024-05-10 18:19:01 +0700

    done tlb

--- src/cpu-tlb.c
+++ src/cpu-tlb.c
@@ -2,43 +2,45 @@
  * Copyright (C) 2024 pdnguyen of the HCMC University of Technology
  */
 /*
- * Source Code License Grant: Authors hereby grants to Licensee 
- * a personal to use and modify the Licensed Source Code for 
+ * Source Code License Grant: Authors hereby grants to Licensee
+ * a personal to use and modify the Licensed Source Code for
  * the sole purpose of studying during attending the course CO2018.
  */
-//#ifdef CPU_TLB
+// #ifdef CPU_TLB
 /*
  * CPU TLB
  * TLB module cpu/cpu-tlb.c
  */
- 
+
 #include "mm.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-int tlb_change_all_page_tables_of(struct pcb_t *proc,  struct memphy_struct * mp)
+#include <pthread.h>
+#ifdef CPU_TLB
+static pthread_mutex_t cpu_tlb = PTHREAD_MUTEX_INITIALIZER;
+int tlb_change_all_page_tables_of(struct pcb_t *proc, struct memphy_struct *mp)
 {
-  /* TODO update all page table directory info 
+  /* TODO update all page table directory info
    *      in flush or wipe TLB (if needed)
    */
 
   return 0;
 }
 
-int tlb_flush_tlb_of(struct pcb_t *proc, struct memphy_struct * mp)
+int tlb_flush_tlb_of(struct pcb_t *proc, struct memphy_struct *mp)
 {
   /* TODO flush tlb cached*/
-
   return 0;
 }
 
 /*tlballoc - CPU TLB-based allocate a region memory
  *@proc:  Process executing the instruction
- *@size: allocated size 
+ *@size: allocated size
  *@reg_index: memory region ID (used to identify variable in symbole table)
  */
 int tlballoc(struct pcb_t *proc, uint32_t size, uint32_t reg_index)
 {
+  pthread_mutex_lock(&cpu_tlb);
   int addr, val;
 
   /* By default using vmaid = 0 */
@@ -46,25 +48,26 @@ int tlballoc(struct pcb_t *proc, uint32_t size, uint32_t reg_index)
 
   /* TODO update TLB CACHED frame num of the new allocated page(s)*/
   /* by using tlb_cache_read()/tlb_cache_write()*/
-
+  TLBMEMPHY_dump(proc->tlb);
+  pthread_mutex_unlock(&cpu_tlb);
   return val;
 }
 
 /*pgfree - CPU TLB-based free a region memory
  *@proc: Process executing the instruction
- *@size: allocated size 
+ *@size: allocated size
  *@reg_index: memory region ID (used to identify variable in symbole table)
  */
 int tlbfree_data(struct pcb_t *proc, uint32_t reg_index)
 {
+  pthread_mutex_lock(&cpu_tlb);
   __free(proc, 0, reg_index);
-
   /* TODO update TLB CACHED frame num of freed page(s)*/
   /* by using tlb_cache_read()/tlb_cache_write()*/
-
+  TLBMEMPHY_dump(proc->tlb);
+  pthread_mutex_unlock(&cpu_tlb);
   return 0;
 }
-
 
 /*tlbread - CPU TLB-based read a region memory
  *@proc: Process executing the instruction
@@ -72,31 +75,57 @@ int tlbfree_data(struct pcb_t *proc, uint32_t reg_index)
  *@offset: source address = [source] + [offset]
  *@destination: destination storage
  */
-int tlbread(struct pcb_t * proc, uint32_t source,
-            uint32_t offset, 	uint32_t destination) 
+int tlbread(struct pcb_t *proc, uint32_t source,
+            uint32_t offset, uint32_t destination)
 {
-  BYTE data, frmnum = -1;
-	
+  BYTE data = -1;
+  int frmnum = -1, val = 0;
   /* TODO retrieve TLB CACHED frame num of accessing page(s)*/
   /* by using tlb_cache_read()/tlb_cache_write()*/
   /* frmnum is return value of tlb_cache_read/write value*/
-	
+  struct vm_rg_struct *region = get_symrg_byid(proc->mm, source);
+  if (check_if_in_freerg_list(proc, 0, region) < 0)
+  {
+    printf("REGION READ NULL\n");
+    return -1;
+  }
+  int addr = region->rg_start + offset;
+
+  int page = PAGING_PGN(addr);
+
+  tlb_cache_read(proc->tlb, proc->pid, page, &frmnum);
 #ifdef IODUMP
   if (frmnum >= 0)
-    printf("TLB hit at read region=%d offset=%d\n", 
-	         source, offset);
-  else 
-    printf("TLB miss at read region=%d offset=%d\n", 
-	         source, offset);
+  {
+    printf("TLB hit at read region=%d offset=(%d\n",
+           source, offset);
+  }
+  else
+  {
+    printf("TLB miss at read region=%d offset=%d\n",
+           source, offset);
+  }
 #ifdef PAGETBL_DUMP
-  print_pgtbl(proc, 0, -1); //print max TBL
+  TLBMEMPHY_dump(proc->tlb);
 #endif
   MEMPHY_dump(proc->mram);
 #endif
 
-  int val = __read(proc, 0, source, offset, &data);
-
-  destination = (uint32_t) data;
+  if (frmnum >= 0)
+  {
+    int physical_addr = (frmnum << PAGING_ADDR_FPN_HIBIT) + offset;
+    MEMPHY_read(proc->mram, physical_addr, &data);
+  }
+  else
+  {
+    val = __read(proc, 0, source, offset, &data);
+    if (val == 0)
+    {
+      tlb_cache_write(proc->tlb, proc->pid, page, data);
+    }
+    TLBMEMPHY_dump(proc->tlb);
+  }
+  destination = (uint32_t)data;
 
   /* TODO update TLB CACHED with frame num of recent accessing page(s)*/
   /* by using tlb_cache_read()/tlb_cache_write()*/
@@ -110,35 +139,63 @@ int tlbread(struct pcb_t * proc, uint32_t source,
  *@destination: index of destination register
  *@offset: destination address = [destination] + [offset]
  */
-int tlbwrite(struct pcb_t * proc, BYTE data,
+int tlbwrite(struct pcb_t *proc, BYTE data,
              uint32_t destination, uint32_t offset)
 {
-  int val;
+  int val = 0;
   BYTE frmnum = -1;
-
   /* TODO retrieve TLB CACHED frame num of accessing page(s))*/
   /* by using tlb_cache_read()/tlb_cache_write()
   frmnum is return value of tlb_cache_read/write value*/
+  struct vm_rg_struct *region = get_symrg_byid(proc->mm, (int)destination);
+  if (region == NULL)
+  {
+    printf("REGION WRITE NULL\n");
+    return -1;
+  }
+
+  int addr = region->rg_start + offset;
+  int page = PAGING_PGN(addr);
+  frmnum = PAGING_FPN(page);
+  // printf("pid: %d page: %d farme: %d 2222222222222222222222222222222\n", proc->pid, page, frmnum);
+  tlb_cache_write(proc->tlb, proc->pid, page, frmnum);
 
 #ifdef IODUMP
   if (frmnum >= 0)
+  {
     printf("TLB hit at write region=%d offset=%d value=%d\n",
-	          destination, offset, data);
-	else
+           destination, offset, data);
+  }
+  else
+  {
     printf("TLB miss at write region=%d offset=%d value=%d\n",
-            destination, offset, data);
+           destination, offset, data);
+  }
 #ifdef PAGETBL_DUMP
-  print_pgtbl(proc, 0, -1); //print max TBL
+  TLBMEMPHY_dump(proc->tlb);
 #endif
   MEMPHY_dump(proc->mram);
 #endif
 
-  val = __write(proc, 0, destination, offset, data);
+  if (frmnum >= 0)
+  {
 
+    int phyaddr = (frmnum << PAGING_ADDR_FPN_LOBIT) + offset;
+    MEMPHY_write(proc->mram, phyaddr, data);
+  }
+  else
+  {
+    val = __write(proc, 0, destination, offset, data);
+    if (val == 0)
+    {
+      // printf("pid: %d page: %d value: %d 333333333333333333333333\n", proc->pid, page, data);
+      tlb_cache_write(proc->tlb, proc->pid, page, data);
+    }
+    TLBMEMPHY_dump(proc->tlb);
+  }
   /* TODO update TLB CACHED with frame num of recent accessing page(s)*/
   /* by using tlb_cache_read()/tlb_cache_write()*/
-
   return val;
 }
 
-//#endif
+#endif
--- src/cpu-tlbcache.c
+++ src/cpu-tlbcache.c
@@ -19,8 +19,23 @@
 
 #include "mm.h"
 #include <stdlib.h>
-
+#define GET_TAG(tlb_page) GETVAL(tlb_page, GENMASK(8, 0), 0)
+#define GET_PID(tlb_page) GETVAL(tlb_page, GENMASK(29, 9), 9)
+#define GET_VALID(tlb_page) GETVAL(tlb_page, BIT(30), 30)
+#define init_tlbcache(mp, sz, ...) init_memphy(mp, sz, (1, ##__VA_ARGS__))
+#define SET_PID(tlb_page, pid) SETVAL(tlb_page, pid, GENMASK(29, 9), 9)
+#define SET_TAG(tlb_page, tag) SETVAL(tlb_page, tag, GENMASK(8, 0), 0)
 #define init_tlbcache(mp,sz,...) init_memphy(mp, sz, (1, ##__VA_ARGS__))
+/*
+bit 31: TAG USED
+bit 30: VALID
+bit 29-9: PID
+bit 8-0: TAG
+
+*/
+#define MAX_TLB 8
+static uint32_t *tlb[MAX_TLB];
+static pthread_mutex_t tlb_lock;
 
 /*
  *  tlb_cache_read read TLB cache device
@@ -29,14 +44,45 @@
  *  @pgnum: page number
  *  @value: obtained value
  */
-int tlb_cache_read(struct memphy_struct * mp, int pid, int pgnum, BYTE value)
+
+/*
+ *  tlb_cache_read read TLB cache device
+ *  @mp: memphy struct
+ *  @pid: process id
+ *  @pgnum: page number
+ *  @value: obtained value
+ */
+
+
+int tlb_clear_tlb_entry(struct memphy_struct *mp, int pid, int pgnum)
+{
+   int index = pgnum % 8;
+   if (GET_PID(tlb[index][0]) == pid)
+   {
+      CLRBIT(tlb[index][0], BIT(30));
+      SET_PID(tlb[index][0], 0);
+      tlb[index][1] = 0;
+   }
+   return 0;
+}
+int tlb_cache_read(struct memphy_struct * mp, int pid, int pgnum, BYTE *value)
 {
    /* TODO: the identify info is mapped to 
     *      cache line by employing:
     *      direct mapped, associated mapping etc.
     */
-   
-   return 0;
+   int index = pgnum%8;
+   int tag = pgnum/8;
+   uint32_t tlb_pg = tlb[index][0];
+   if(tlb_pg&&BIT(30)){
+      int tag_pg = GET_TAG(tlb_pg);
+      int pid_pg = GET_PID(tlb_pg);
+      if(tag==tag_pg&&pid==pid_pg){
+         *value = (int)tlb[index][1];
+         return 0 ;
+      }
+      return -1;
+   }
 }
 
 /*
@@ -52,7 +98,15 @@ int tlb_cache_write(struct memphy_struct *mp, int pid, int pgnum, BYTE value)
     *      cache line by employing:
     *      direct mapped, associated mapping etc.
     */
-   return 0;
+
+   int index = pgnum % 8;
+   int tag = pgnum / 8;
+   tlb[index][1] = value;
+   SET_TAG(tlb[index][0], tag);
+   SET_PID(tlb[index][0], pid);
+   SETBIT(tlb[index][0], BIT(30));
+
+   return -1;
 }
 
 /*
@@ -102,6 +156,17 @@ int TLBMEMPHY_dump(struct memphy_struct * mp)
     *     for tracing the memory content
     */
 
+  pthread_mutex_lock(&tlb_lock);
+   printf("-----------------------Dump TLB------------------------\n");
+   for (int i = 0; i < MAX_TLB; i++)
+   {
+      int valid = GET_VALID(tlb[i][0]);
+      int pid = GET_PID(tlb[i][0]);
+      int frame = tlb[i][1];
+      printf("%02d %d %08d %08d\n", i, valid, pid, frame);
+   }
+   printf("-----------------------Dump TLB------------------------\n");
+   pthread_mutex_unlock(&tlb_lock);
    return 0;
 }
 
@@ -112,10 +177,16 @@ int TLBMEMPHY_dump(struct memphy_struct * mp)
 int init_tlbmemphy(struct memphy_struct *mp, int max_size)
 {
    mp->storage = (BYTE *)malloc(max_size*sizeof(BYTE));
+   for (int i = 0; i < MAX_TLB; i++)
+   {
+      tlb[i] = (uint32_t *)(mp->storage + i * 8);
+      tlb[i][0] = 0; 
+      tlb[i][1] = 0;
+   }
    mp->maxsz = max_size;
 
    mp->rdmflg = 1;
-
+   pthread_mutex_init(&tlb_lock, NULL);
    return 0;
 }
 
